2d484d8d13544722b4689864d4b5f58522da425f
ralfsegle <egleralfs713@gmail.com>
2025-04-21 14:37:44 +0300

    Access properties and methods in OOP PHP

--- car.php
+++ car.php
@@ -4,12 +4,18 @@ class Car {
  //properties / fields
     private $brand;
     private $colour;
-    private $trim = "coupe";
+    public $trim = "coupe";
 
     //Constructor
-    public function __construct($brand, $colour) {
+    public function __construct($brand, $colour = "none") {
         $this ->brand = $brand;
         $this ->colour = $colour;
     }
 
 }
+
+$car01 = new Car("Audi", "black");
+echo $car01->trim;
+$car02 = new Car("Renault", "white");
+$car03 = new Car("Subaru", "blue");
+$car4 = new Car("Ford");
